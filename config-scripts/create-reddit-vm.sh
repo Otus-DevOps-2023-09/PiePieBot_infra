e7e0ceb941d480c6742113941e661c76d1cf4d99
PiePieBot <kazulka123@bk.ru>
2023-11-05 16:09:51 +0000

    conflict fix

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -5,6 +5,6 @@ yc compute instance create \
 	--hostname reddit-app \
 	--memory=4  \
 	--create-boot-disk name=reddit-full,image-id=$IMAGE_ID,size=10GB \
-        --network-interface subnet-name=my-net-ru-central1-a,nat-ip-version=ipv4 \
+        --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
 	--metadata serial-port-enable=1 \
 	--ssh-key ~/.ssh/appuser.pub
